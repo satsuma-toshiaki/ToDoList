83d11433c675cc0b298c53c348648f09056c25c5
satsuma-toshiaki <satsuma-toshiaki@outlook.jp>
2016-08-22 13:25:03 +0900

    second

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713013317) do
+ActiveRecord::Schema.define(version: 20160713014148) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "categoryname"
